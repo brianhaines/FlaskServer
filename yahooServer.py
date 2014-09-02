456eb9af374ec9c941d6ab225292d749734f5da0
DevOnLinux <b.ge.haines@gmail.com>
2014-09-02 15:16:06 -0400

    Flask Hello World.

--- yahooServer.py
+++ yahooServer.py
@@ -5,17 +5,23 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 # create our little application :)
 app = Flask(__name__)
 
-app.config.update(dict(
-	DATABASE = os.path.join(app.root_path, 'flaskr.db'),
-	DEBUG = True,
-	SECRET_KEY = 'development key',
-	USERNAME = 'admin',
-	PASSWORD = 'default'
-))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
 	port=int(os.environ.get('PORT', 5000))
 	app.run(port=port)
+
+
+
+# app.config.update(dict(
+# 	DATABASE = os.path.join(app.root_path, 'flaskr.db'),
+# 	DEBUG = True,
+# 	SECRET_KEY = 'development key',
+# 	USERNAME = 'admin',
+# 	PASSWORD = 'default'
+# ))
+# app.config.from_envvar('FLASKR_SETTINGS', silent=True)
