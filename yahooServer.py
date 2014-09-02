16cca81675733fbb809bfd882e966cf46435d2e5
DevOnLinux <b.ge.haines@gmail.com>
2014-09-02 13:43:17 -0400

    First commit.

---
+++ yahooServer.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+
+# create our little application :)
+app = Flask(__name__)
+
+app.config.update(dict(
+	DATABASE = os.path.join(app.root_path, 'flaskr.db'),
+	DEBUG = True,
+	SECRET_KEY = 'development key',
+	USERNAME = 'admin',
+	PASSWORD = 'default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+
+if __name__ == '__main__':
+	port=int(os.environ.get('PORT', 5000))
+	app.run(port=port)
