1f35d1b5be205074528b051853c5098dbe1e7d6a
DevOnLinux <b.ge.haines@gmail.com>
2014-09-02 17:51:49 -0400

    Added a query to the RDS MySQL database.

--- yahooServer.py
+++ yahooServer.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python3
 import os
+import mysql.connector as mysql
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+#This is a text file with my db username and PW etc.
+params = open('dbparams.txt').read()
+#params = open('/home/ubuntu/Yahoo/dbparams.txt').read()
+params =  params.split(',')
 
 
 # create our little application :)
@@ -9,25 +16,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+	return 'Hello World!'
 
 @app.route('/<ticker>')
 def stock_tip(ticker):
-    # Hot stock tip here:
-    return '%s is so hot right now.' % ticker
+	# Hot stock tip here:
+	tkr = ticker
+	db = mysql.connect(user=params[0],password=params[1],host=params[2],database=params[3])
+	cursor = db.cursor()
+
+	cursor.execute('''SELECT tBucket, avg(tPrice) FROM livePrices WHERE Ticker=%s AND qDate='2014-09-02' AND tPrice!='Null' GROUP BY tBucket ORDER BY qTime DESC LIMIT 1''',(tkr,))
+
+	for each in cursor:
+		outPut = [str(each[0]),each[1]]
+	
+	return '%s is %s at %s' % (tkr,outPut[1],outPut[0])
 
 
 if __name__ == '__main__':
 	port=int(os.environ.get('PORT', 5000))
 	app.run(port=port)
 
-
-
-# app.config.update(dict(
-# 	DATABASE = os.path.join(app.root_path, 'flaskr.db'),
-# 	DEBUG = True,
-# 	SECRET_KEY = 'development key',
-# 	USERNAME = 'admin',
-# 	PASSWORD = 'default'
-# ))
-# app.config.from_envvar('FLASKR_SETTINGS', silent=True)
