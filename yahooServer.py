c38288312069aa084647a80c621f2c6f66317b23
DevOnLinux <b.ge.haines@gmail.com>
2014-09-03 12:34:42 -0400

    Refactored to give the desired number of responses.

--- yahooServer.py
+++ yahooServer.py
@@ -2,6 +2,7 @@
 import os
 import mysql.connector as mysql
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+import json
 
 #This is a text file with my db username and PW etc.
 params = open('dbparams.txt').read()
@@ -17,19 +18,30 @@ app = Flask(__name__)
 def hello_world():
 	return 'Hello World!'
 
-@app.route('/<ticker>')
-def stock_tip(ticker):
-	# Hot stock tip here:
-	tkr = ticker
+@app.route('/prices/<stkReq>')
+def stock_tip(stkReq):
+	s = stkReq.split('&')
+	
+	tkr = s[0]
+	n = int(s[1])
+	qDate = '2014-09-03'
+	
 	db = mysql.connect(user=params[0],password=params[1],host=params[2],database=params[3])
 	cursor = db.cursor()
 
-	cursor.execute('''SELECT tBucket, avg(tPrice),max(tPrice),min(tPrice) FROM livePrices WHERE Ticker=%s AND qDate='2014-09-03' AND tPrice!='Null' GROUP BY tBucket ORDER BY qTime DESC LIMIT 1''',(tkr,))
+	cursor.execute('''SELECT qDate, tBucket, avg(tPrice),max(tPrice),min(tPrice) FROM livePrices WHERE Ticker=%s AND qDate=%s AND tPrice!='Null' GROUP BY tBucket ORDER BY qTime DESC LIMIT %s''',(tkr,qDate,n))
 
+	outPut=[]
 	for each in cursor:
-		outPut = [str(each[0]),round(each[1],2),round(each[2],2),round(each[3],2)]
-	
-	return '%s Price: avg: %s hi: %s lo: %s  at %s' % (tkr,outPut[1],outPut[2],outPut[3],outPut[0],)
+		outPut.append([str(each[0]),str(each[1]),round(each[2],2),round(each[3],2),round(each[4],2)])
+
+	dats = {'Ticker':tkr,'Prices':outPut}
+	return json.dumps(dats,separators=(',', ':'))
+
+
+@app.route('/keystats/<stkReq>')
+def stock_tip(stkReq):
+	return "YEAH BUDDY!"
 
 
 if __name__ == '__main__':
